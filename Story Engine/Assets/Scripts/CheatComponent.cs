6fde1ae0664197d296322fe5474514eeee53077f
Jake North <jaynoco@gmail.com>
2019-10-14 20:34:39 -0500

    Add Cheat Component, Script.

---
+++ Story Engine/Assets/Scripts/CheatComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatComponent : MonoBehaviour {
+    public VictoryCoach myVictoryCoach;
+
+    // Use this for initialization
+    void Start () {
+        myVictoryCoach = GameObject.FindObjectOfType<VictoryCoach>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            myVictoryCoach.achieveNextExperience();
+        }
+	}
+}
