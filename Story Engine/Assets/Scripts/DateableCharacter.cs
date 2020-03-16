ef6a353596740685bf43e799e544747e8dff8195
Jake North <jaynoco@gmail.com>
2020-03-16 14:53:32 -0400

    Scene updates to Characters.

--- Story Engine/Assets/Scripts/DateableCharacter.cs
+++ Story Engine/Assets/Scripts/DateableCharacter.cs
@@ -57,7 +57,7 @@ public class DateableCharacter : Character {
 
 			dm.registerDialogue(new DialoguePiece("Um.. I have to go over there now.", this).addTag("departure").addTag("deflection"));
 			dm.registerDialogue(new DialoguePiece("Um.. I have to be somewhere.", this).addTag("departure").addTag("time"));
-		}else if (givenName.ToLower() == "sara")
+		}else if (givenName.ToLower() == "sarah")
         {
             dm.registerDialogue(new DialoguePiece("HELLO.", this).addTag("greeting"));
             dm.registerDialogue(new DialoguePiece("You again...", this).addTag("greeting").addTag("annoyed"));
@@ -135,7 +135,7 @@ public class DateableCharacter : Character {
 
             dm.registerDialogue(new DialoguePiece("Um.. I have to go over there now.", this).addTag("departure").addTag("deflection"));
             dm.registerDialogue(new DialoguePiece("Um.. I have to be somewhere.", this).addTag("departure").addTag("time"));
-		}else if (givenName.ToLower() == "olivia")
+		}else if (givenName.ToLower() == "eleanor")
         {
             dm.registerDialogue(new DialoguePiece("HELLO.", this).addTag("greeting"));
             dm.registerDialogue(new DialoguePiece("You again...", this).addTag("greeting").addTag("annoyed"));
@@ -255,46 +255,6 @@ public class DateableCharacter : Character {
             dm.registerDialogue(new DialoguePiece("Um.. I have to go over there now.", this).addTag("departure").addTag("deflection"));
             dm.registerDialogue(new DialoguePiece("Um.. I have to be somewhere.", this).addTag("departure").addTag("time"));
         }
-        else if (givenName.ToLower() == "sarah")
-        {
-            dm.registerDialogue(new DialoguePiece("HELLO.", this).addTag("greeting"));
-            dm.registerDialogue(new DialoguePiece("You again...", this).addTag("greeting").addTag("annoyed"));
-
-            dm.registerDialogue(new DialoguePiece("Nice to meet you.", this).addTag("introduction"));
-            dm.registerDialogue(new DialoguePiece("Hi again!", this).addTag("introduction").addTag("known"));
-            dm.registerDialogue(new DialoguePiece("I heard you the first time.", this).addTag("introduction").addTag("repeated"));
-
-            dm.registerDialogue(new DialoguePiece("I'm busy. Come back later.", this).addTag("comebacklater"));
-            dm.registerDialogue(new DialoguePiece("Sure! I'd love to!", this).addTag("date.yes"));
-            dm.registerDialogue(new DialoguePiece("I'm busy... I have to baby-sit...", this).addTag("date.no"));
-            dm.registerDialogue(new DialoguePiece("You'll have to catch me with it.", this).addTag("demand.yes").addTag("affectionate"));
-            dm.registerDialogue(new DialoguePiece("Fine. Here you go.", this).addTag("demand.yes").addTag("unaffectionate"));
-            dm.registerDialogue(new DialoguePiece("I'll come back and get it for you later.", this).addTag("demand.no").addTag("affectionate"));
-            dm.registerDialogue(new DialoguePiece("Shove your stapler up your ass.", this).addTag("demand.no").addTag("unaffectionate"));
-
-            dm.registerDialogue(new DialoguePiece("It's over there.", this).addTag("question"));
-            dm.registerDialogue(new DialoguePiece("I'm not sure.", this).addTag("question").addTag("indifferent"));
-            dm.registerDialogue(new DialoguePiece("I don't know you, and you already asked me that.", this).addTag("question").addTag("bothered"));
-            dm.registerDialogue(new DialoguePiece("I'd be glad to help you.", this).addTag("question").addTag("helpful"));
-            dm.registerDialogue(new DialoguePiece("It's over there.", this).addTag("question").addTag("willing"));
-
-            dm.registerDialogue(new DialoguePiece("Thank you.", this).addTag("compliment"));
-            dm.registerDialogue(new DialoguePiece("Thanks, I guess.", this).addTag("compliment").addTag("positive"));
-            dm.registerDialogue(new DialoguePiece("I get it.", this).addTag("compliment").addTag("indifferent"));
-            dm.registerDialogue(new DialoguePiece("You're making me blush!", this).addTag("compliment").addTag("hot"));
-            dm.registerDialogue(new DialoguePiece("Why thank you.", this).addTag("compliment").addTag("warm"));
-            dm.registerDialogue(new DialoguePiece("Oh, stop!", this).addTag("compliment").addTag("embarrassed"));
-
-            dm.registerDialogue(new DialoguePiece("You're very demanding.", this).addTag("demand"));
-            dm.registerDialogue(new DialoguePiece("Okay.", this).addTag("demand").addTag("subordinate"));
-            dm.registerDialogue(new DialoguePiece("Maybe later.", this).addTag("demand").addTag("unwilling"));
-            dm.registerDialogue(new DialoguePiece("Shove it up your ass.", this).addTag("demand").addTag("insubordinate"));
-            dm.registerDialogue(new DialoguePiece("Yes sir.", this).addTag("demand").addTag("compliant"));
-            dm.registerDialogue(new DialoguePiece("Get it yourself.", this).addTag("demand").addTag("incompliant"));
-
-            dm.registerDialogue(new DialoguePiece("Um.. I have to go over there now.", this).addTag("departure").addTag("deflection"));
-            dm.registerDialogue(new DialoguePiece("Um.. I have to be somewhere.", this).addTag("departure").addTag("time"));
-        }
         else if (givenName.ToLower() == "jasmine")
         {
             dm.registerDialogue(new DialoguePiece("HELLO.", this).addTag("greeting"));
@@ -375,7 +335,7 @@ public class DateableCharacter : Character {
             dm.registerDialogue(new DialoguePiece("Um.. I have to go over there now.", this).addTag("departure").addTag("deflection"));
             dm.registerDialogue(new DialoguePiece("Um.. I have to be somewhere.", this).addTag("departure").addTag("time"));
         }
-        else if (givenName.ToLower() == "melissa")
+        else if (givenName.ToLower() == "missy")
         {
             dm.registerDialogue(new DialoguePiece("HELLO.", this).addTag("greeting"));
             dm.registerDialogue(new DialoguePiece("You again...", this).addTag("greeting").addTag("annoyed"));
